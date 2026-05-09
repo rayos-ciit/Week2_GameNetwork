2a3707da61280770b73eb80e063e12e6d9129922
Myst <janrein.rayos@ciit.edu.ph>
2026-05-10 00:35:11 +0800

    adding of playercount and ui polishj

--- Assets/Script/MultiplayerMenu.cs
+++ Assets/Script/MultiplayerMenu.cs
@@ -3,18 +3,31 @@ using UnityEngine;
 
 public class MultiplayerMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject menuUI; // Drag your UI Panel here in the Inspector
+
     public void StartHost()
     {
         NetworkManager.Singleton.StartHost();
+        HideUI();
     }
 
     public void StartClient()
     {
         NetworkManager.Singleton.StartClient();
+        HideUI();
     }
 
     public void StartServer()
     {
         NetworkManager.Singleton.StartServer();
+        HideUI();
+    }
+
+    private void HideUI()
+    {
+        if (menuUI != null)
+        {
+            menuUI.SetActive(false);
+        }
     }
 }
